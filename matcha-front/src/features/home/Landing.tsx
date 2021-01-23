41c65ed977e404d36ed779801c3645ca72514a18
Pekka Tuukkanen <petuukka@gmail.com>
2021-01-23 18:31:37 +0200

    Register Modal

--- matcha-front/src/features/home/Landing.tsx
+++ matcha-front/src/features/home/Landing.tsx
@@ -1,20 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { Button, Header, Menu } from 'semantic-ui-react';
+import Register from '../user/Register';
 
 const Landing = () => {
+	const [registerOpen, setRegisterOpen] = useState(false);
+
+	const closeRegisterModal = () => {
+		setRegisterOpen(false);
+	};
+
+	const openRegisterModal = () => {
+		setRegisterOpen(true);
+	};
+
 	return (
 		<Fragment>
 			<Menu fixed="top" icon="labeled" size="mini" borderless className="navi">
-					<Menu.Item header style={{fontSize: 32, padding: 0}}>Matcha</Menu.Item>
-					<Menu.Item position="right">
-						<Button size="tiny" secondary content="Login" />
-					</Menu.Item>
-					<Menu.Item>
-						<Button size='huge' secondary content="Register" />
-					</Menu.Item>
-
+				<Menu.Item header style={{ fontSize: 32, padding: 0 }}>
+					Matcha
+				</Menu.Item>
+				<Menu.Item position="right">
+					<Button size="tiny" secondary content="Login" />
+				</Menu.Item>
+				<Menu.Item>
+					<Button
+						size="huge"
+						secondary
+						content="Register"
+						onClick={openRegisterModal}
+					/>
+				</Menu.Item>
 			</Menu>
 			<Header>Welcome to matcha</Header>
+			<Register open={registerOpen} closeRegisterModal={closeRegisterModal} />
 		</Fragment>
 	);
 };
