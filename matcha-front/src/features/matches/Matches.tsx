b6cbd4798bb63cc17080a4bdf19123bbb2b12c2c
Antti Vornanen <anttivornanen@Antti-iMac.local>
2021-01-25 13:21:15 +0200

    Add chat button to new match

--- matcha-front/src/features/matches/Matches.tsx
+++ matcha-front/src/features/matches/Matches.tsx
@@ -1,6 +1,14 @@
-import React from 'react'
-import { Container, Card, Image, Header, Label } from 'semantic-ui-react'
-import { IUser } from '../../app/models/user'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+	Container,
+	Card,
+	Image,
+	Header,
+	Label,
+	Button,
+} from 'semantic-ui-react';
+import { IUser } from '../../app/models/user';
 
 interface IProps {
 	users: IUser[];
@@ -12,21 +20,36 @@ const Matches: React.FC<IProps> = ({ users }) => {
 			<Card.Group itemsPerRow={1}>
 				{users.map((user) => (
 					<Card key={user.location.latitude - Date.now()}>
-						<Label color={Math.abs(Math.floor(user.location.latitude)) < 40 ? 'red' : 'green' } floating>
-							{Math.abs(Math.floor(user.location.latitude)) < 40 ? 'Offline' : 'Online' }
+						<Label
+							color={
+								Math.abs(Math.floor(user.location.latitude)) <
+								40
+									? 'red'
+									: 'green'
+							}
+							floating
+						>
+							{Math.abs(Math.floor(user.location.latitude)) < 40
+								? 'Offline'
+								: 'Online'}
 						</Label>
-						<Image src={`https://robohash.org/${user.firstName}`} wrapped ui={false} />
-						<Header as="h5">
-							{user.firstName} {user.lastName}
-						</Header>
+						<Image
+							src={`https://robohash.org/${user.firstName}`}
+							wrapped
+							ui={false}
+						/>
 						<Card.Content>
+							<Card.Header as="h5">
+								{user.firstName} {user.lastName}
+							</Card.Header>
 							Distance: {Math.floor(Math.random() * 80)} km
 						</Card.Content>
+						<Button color="red" icon="heart" content="Start Chat" as={Link} to={'/chat'}/>
 					</Card>
 				))}
 			</Card.Group>
 		</Container>
-	)
-}
+	);
+};
 
-export default Matches
+export default Matches;
