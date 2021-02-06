37b266d78d7093cfe8d9670d0a5e2abfd587cb71
Pekka Tuukkanen <petuukka@gmail.com>
2021-02-06 14:22:30 +0200

    password hash, login, unauthorizederror

--- matcha-backend/src/application/user/login.rs
+++ matcha-backend/src/application/user/login.rs
@@ -1,17 +1,20 @@
 
-use crate::errors::{AppError, LoginError};
+use crate::errors::{AppError};
 use crate::models::user::{LoginFormValues, User};
 use crate::database::cursor::{CursorRequest};
 
 pub async fn login(values: LoginFormValues) -> Result<(), AppError> {
-	let result = CursorRequest::from(format!("FOR u IN users filter u.email_address == '{}' filter u.password == '{}' return u", values.email_address, values.password))
+	let mut result = CursorRequest::from(format!("FOR u IN users filter u.email_address == '{}'return u", values.email_address))
 		.send()
 		.await?
 		.extract_all::<User>()
 		.await?;
-	if result.is_empty() {
-		return Err(AppError::LoginError(LoginError::new("Login failed")))
+	if let Some(user) = result.pop() {
+		if !user.verify_pw(&values.password) {
+			return Err(AppError::unauthorized("Login failed"));
+		}
+	} else {
+		return Err(AppError::unauthorized("Login failed"));
 	}
-
 	Ok(())
 }
