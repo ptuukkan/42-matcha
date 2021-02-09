f65b908c8940f6ed706e3a257d0c664b2b690e47
Antti Vornanen <anttivornanen@Antti-iMac.local>
2021-02-09 15:38:00 +0200

    removed nested if conditions

--- matcha-backend/src/application/user/login.rs
+++ matcha-backend/src/application/user/login.rs
@@ -10,12 +10,12 @@ pub async fn login(values: LoginFormValues) -> Result<(), AppError> {
 		.extract_all::<User>()
 		.await?;
 	if let Some(user) = result.pop() {
-		if user.link == "" {
+			if !user.link == "" {
+				return Err(AppError::unauthorized("Please verify your account!"));
+			}
 			if !user.verify_pw(&values.password) {
 				return Err(AppError::unauthorized("Login failed"));
 			}
-		} else {
-			return Err(AppError::unauthorized("Please verify your account!"));
 		}
 	} else {
 		return Err(AppError::unauthorized("Login failed"));
