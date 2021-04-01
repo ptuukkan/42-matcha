0287a69121370bbc23b2bca1382df1be1018494b
Pekka Tuukkanen <petuukka@gmail.com>
2021-04-01 14:15:16 +0300

    remove unused import

--- matcha-frontend/src/features/profile/ProfilePage.tsx
+++ matcha-frontend/src/features/profile/ProfilePage.tsx
@@ -1,7 +1,6 @@
 import { observer } from 'mobx-react-lite';
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { useEffect } from 'react';
-import { Calendar } from 'react-widgets';
 import {
 	Item,
 	Button,
